fbc3f0dcaf3327c7109b26b97039b3b2f8881da5
RaceWilliams3 <willr108@my.erau.edu>
2022-04-04 22:50:12 -0700

    Added Title

---
+++ Oregon_Trail/Oregon_Trail/Title.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+void titleScreen()
+{
+	cout << "+-----------------------------------+" << endl;
+	cout << "|           Oregon Trail            |" << endl;
+	cout << "|                                   |" << endl;
+	cout << "| Race Williams     Matthew Shepard |" << endl;
+	cout << "+-----------------------------------+" << endl;
+}
